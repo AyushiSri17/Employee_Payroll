f5a2f14b46876d1d109669ecc8a21ea2b68ec3ee
“AyushiSri17” <srivastavaayushi7110@gmail.com>
2023-03-07 00:36:16 +0530

    [Ayushi] Added Create Table

--- Employee_Payroll.sql
+++ Employee_Payroll.sql
@@ -1,2 +1,13 @@
+--------------UC-1------------------
 Create database Payroll_Services;
 SELECT * FROM master.sys.databases;
+Use Payroll_Services;
+
+--------------UC-2------------------
+Create table Employee_Payroll
+(
+Id int Identity(1,1) primary key,
+Name varchar(100),
+Salary float,
+StartDate date
+);
