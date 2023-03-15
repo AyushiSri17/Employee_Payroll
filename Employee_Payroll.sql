aa7651a0173abe27a576a5213279ac44498c741e
“AyushiSri17” <srivastavaayushi7110@gmail.com>
2023-03-15 21:55:09 +0530

    [Ayushi] Added Aggregate function

--- Employee_Payroll.sql
+++ Employee_Payroll.sql
@@ -22,22 +22,25 @@ insert into Employee_PayRoll (Salary, Name, startDate) values (12346, 'Ram', GET
 ----------------UC-4-----------------
 select * from Employee_Payroll;
 
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
->>>>>>> 37254fe84220fb85e2cf60c66245f446dae9c95a
 ----------------UC-5-----------------
 select * from Employee_PayRoll where Name='Raju';
 select * from Employee_Payroll where StartDate BETWEEN '2020-01-01' AND getdate();
 select * from Employee_Payroll where StartDate between CAST('2023-01-01' as date) and GETDATE(); /*Cast method is used for conversion*/
-<<<<<<< HEAD
 
 ----------------UC-6-----------------
 
 ALTER TABLE Employee_Payroll ADD Gender char(1);
 UPDATE Employee_Payroll SET Gender = 'M' where Name = 'Vasu' or Name = 'Raju' or Name = 'Ram';
 UPDATE Employee_Payroll set Gender = 'F' where Name = 'Ayushi' or Name = 'Anjuli';
-=======
->>>>>>> UC5-Retrieve_ParticularEmployeeInfo
->>>>>>> 37254fe84220fb85e2cf60c66245f446dae9c95a
+
+----------------UC-7------------------
+select Count(Id) As NumOfEmployee from Employee_Payroll;
+select Count(Id) As NumOfEmployee, Gender from Employee_Payroll group by Gender;
+select Sum(Salary) As TotalSalary from Employee_Payroll;
+select Sum(Salary) As TotalSalary, Gender from Employee_Payroll group by Gender;
+select Max(Salary) As maxSalary from Employee_Payroll;
+select Max(Salary) As maxSalary, Gender from Employee_Payroll group by Gender;
+select Min(Salary) As minSalary, Gender from Employee_Payroll group by Gender;
+select Min(Salary) As minSalary from Employee_Payroll;
+select Avg(Salary) As avgSalary, Gender from Employee_Payroll group by Gender;
+select Avg(Salary) As avgSalary from Employee_Payroll;
