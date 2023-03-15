f6ce3bdf25ec80ea3865cde08898a22f6884e6b6
“AyushiSri17” <srivastavaayushi7110@gmail.com>
2023-03-15 20:33:38 +0530

    [Ayushi] Added select query to retrieve particular employee information from table

--- Employee_Payroll.sql
+++ Employee_Payroll.sql
@@ -19,3 +19,10 @@ insert into Employee_Payroll values('Vasu',126781,'2019-03-01');
 insert into Employee_Payroll (Salary, Name, StartDate) values (465789, 'Raju', getdate());
 insert into Employee_PayRoll (Salary, Name, startDate) values (12346, 'Ram', GETDATE());
 
+----------------UC-4-----------------
+select * from Employee_Payroll;
+
+----------------UC-5-----------------
+select * from Employee_PayRoll where Name='Raju';
+select * from Employee_Payroll where StartDate Between '2020-01-01' AND getdate();
+select * from Employee_Payroll where StartDate between CAST('2023-01-01' as date) and GETDATE(); /*Cast method is used for conversion*/
