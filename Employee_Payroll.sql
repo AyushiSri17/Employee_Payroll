ba175cdc34cec1e6212ec819735da7fee0a0fe1f
“AyushiSri17” <srivastavaayushi7110@gmail.com>
2023-03-15 19:53:49 +0530

    [Ayushi] Added information into table using insert query

--- Employee_Payroll.sql
+++ Employee_Payroll.sql
@@ -11,3 +11,12 @@ Name varchar(100),
 Salary float,
 StartDate date
 );
+
+----------------UC-3-----------------
+insert into Employee_Payroll values('Ayushi',107321,'2021-04-01');
+insert into Employee_Payroll values('Anjuli',96671,'2013-05-10');
+insert into Employee_Payroll values('Vasu',126781,'2019-03-01');
+insert into Employee_Payroll (Salary, Name, StartDate) values (465789, 'Raju', getdate());
+insert into Employee_PayRoll (Salary, Name, startDate) values (12346, 'Ram', GETDATE());
+
+
